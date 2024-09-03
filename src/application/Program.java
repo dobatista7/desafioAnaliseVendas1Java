ade6c2bccff12ca9a5779bac8cc9313a116fb2d6
Douglas Batista <dobatista7@gmail.com>
2024-09-02 21:19:31 -0300

    Leitura do Arquivo CSV,  criação da lista de vendas no bloco Try Catch

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,40 @@
 package application;
 
+import entities.Sale;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Entre o caminho do arquivo: ");
+        String path = sc.nextLine();
+
+        // Leitura do Arquivo CSV,  criação da lista de vendas no bloco Try Catch
+        List<Sale> sales = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(path))) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                Integer month = Integer.parseInt(fields[0]);
+                Integer year = Integer.parseInt(fields[1]);
+                String seller = fields[2];
+                Integer items = Integer.parseInt(fields[3]);
+                Double total = Double.parseDouble(fields[4]);
+
+                sales.add(new Sale(month, year, seller, items, total));
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage() + " (O sistema não pode encontrar o arquivo especificado) ");
+            return; // Encerra o programa em caso de erro na leitura do arquivo
         }
     }
+}
